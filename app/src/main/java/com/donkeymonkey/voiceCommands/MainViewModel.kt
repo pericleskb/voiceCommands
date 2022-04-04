1550d7dbcd386b537952c03f3dac1f15506072d1
perikliskb <pericleskb@gmail.com>
2022-04-04 23:29:04 +0300

    add add button in adapter

--- app/src/main/java/com/donkeymonkey/voiceCommands/MainViewModel.kt
+++ app/src/main/java/com/donkeymonkey/voiceCommands/MainViewModel.kt
@@ -5,23 +5,24 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.donkeymonkey.voicecommandsdata.db.VoiceCommandsDatabase
+import com.donkeymonkey.voicecommandsdata.entities.Command
 import com.donkeymonkey.voicecommandsdata.repositories.CommandRepositoryImpl
 
 class MainViewModel(context: Context): ViewModel() {
     private val commandsRepository = CommandRepositoryImpl(
         VoiceCommandsDatabase.getDatabase(context).commandDao()
     )
-    private val buttons: MutableLiveData<ArrayList<String>> by lazy {
-        MutableLiveData<ArrayList<String>>().also {
+    private val buttons: MutableLiveData<List<Command>> by lazy {
+        MutableLiveData<List<Command>>().also {
             loadButtons()
         }
     }
 
-    private fun loadButtons() {
-
+    private suspend fun loadButtons() {
+        buttons.postValue(commandsRepository.getCommands().value)
     }
 
-    fun getButtons(): LiveData<ArrayList<String>> {
+    fun getButtons(): LiveData<List<Command>> {
         return buttons
     }
 }
--- app/src/main/java/com/donkeymonkey/voiceCommands/commandsFragment/CommandsRecyclerViewAdapter.kt
+++ app/src/main/java/com/donkeymonkey/voiceCommands/commandsFragment/CommandsRecyclerViewAdapter.kt
@@ -5,42 +5,73 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.donkeymonkey.voiceCommands.R
+import com.donkeymonkey.voiceCommands.databinding.LayoutAddCommandButtonBinding
 import com.donkeymonkey.voiceCommands.databinding.LayoutCommandSquareBinding
+import com.donkeymonkey.voicecommandsdata.entities.Command
 
-class CommandsRecyclerViewAdapter(): RecyclerView.Adapter<CommandsRecyclerViewAdapter.CommandViewHolder>() {
+class CommandsRecyclerViewAdapter(): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
-    private val commandsList: ArrayList<String> = ArrayList()
+    private val commandsList: ArrayList<Command> = ArrayList()
+
+    private val ADD_BUTTON_TYPE = 0;
+    private val COMMAND_TYPE = 1;
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CommandViewHolder {
+        if (viewType == ADD_BUTTON_TYPE) {
+            return
+        }
         return CommandViewHolder(
-            LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_command_square, parent, false)
+            LayoutInflater.from(parent.context).inflate(R.layout.layout_command_square, parent, false)
         )
     }
 
-    override fun onBindViewHolder(holder: CommandViewHolder, position: Int) {
-        holder.bind(commandsList[position])
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (holder) {
+            is AddButtonViewHolder -> {
+                holder.bind()
+            }
+            is CommandViewHolder -> {
+                holder.bind(commandsList[position])
+            }
+        }
     }
 
     override fun getItemCount(): Int {
-        return commandsList.size
+        return commandsList.size + 1
     }
 
-    fun setButtons(texts: ArrayList<String>) {
+    override fun getItemViewType(position: Int): Int {
+        return if (commandsList.isEmpty() || position == commandsList.size) {
+            ADD_BUTTON_TYPE
+        } else {
+            COMMAND_TYPE
+        }
+    }
+
+    fun setButtons(commands: List<Command>) {
         commandsList.clear()
-        commandsList.addAll(texts)
+        commandsList.addAll(commands)
         notifyDataSetChanged()
     }
 
-    fun addButton(text: String) {
-        commandsList.add(text)
+    fun addButton(command: Command) {
+        commandsList.add(command)
         notifyItemChanged(commandsList.size -1)
     }
 
     class CommandViewHolder(view: View): RecyclerView.ViewHolder(view) {
-        val binding = LayoutCommandSquareBinding.bind(view)
+        private val binding = LayoutCommandSquareBinding.bind(view)
 
-        fun bind(text: String) {
-            binding.root.text = text
+        fun bind(command: Command) {
+            binding.root.text = command.name
+        }
+    }
+
+    class AddButtonViewHolder(view: View): RecyclerView.ViewHolder(view) {
+        private val binding = LayoutAddCommandButtonBinding.bind(view)
+
+        fun bind() {
+            binding.root.text = "+"
         }
     }
 }
